0b972fbd38daa0edef975ac309e630fe29f774b1
Kevin George Buhain <keving.buhain@gmail.com>
2026-04-25 14:59:39 -0400

    Added first and last name prompts

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Declare variables
 // collect age and name
-string name = string.Empty;
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 65;
 
 // Prompt the user for input
-Console.WriteLine("please enter your name");
-name = Console.ReadLine();
+Console.WriteLine("please enter your first name");
+firstName = Console.ReadLine();
+
+Console.WriteLine("please enter your last name");
+lastName = Console.ReadLine();
 
 Console.WriteLine("please enter your name");
 age = Convert.ToInt32(Console.ReadLine());
@@ -16,6 +20,6 @@ int workingYearsRemaining = retirementAge - age;
 
 
 // Output the results to the user
-Console.WriteLine($"Full Name: {name}");
+Console.WriteLine($"Full Name: {firstName} {lastName");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"Working Years Remaining: {workingYearsRemaining}");
