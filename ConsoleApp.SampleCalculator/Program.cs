88093598aa2c7b6b06c7b113e9454b053dd8ad2e
Kevin George Buhain <keving.buhain@gmail.com>
2026-04-27 22:11:25 -0400

    Added Exception Handling and Modified the Calculator

--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,5 +1,4 @@
-﻿
-// Welcome Message
+﻿// Welcome Message
 Console.WriteLine("********************* Welcome to the Sample Calculator *********************");
 
 
@@ -7,99 +6,119 @@ Console.WriteLine("********************* Welcome to the Sample Calculator ******
 /*
  * Math Operations and operators
  */
-
-// Show calculator options / Show menu
-Console.WriteLine("Please select an operation (-1 to exit program)");
-Console.WriteLine("1. Addition");
-Console.WriteLine("2. Subtraction");
-Console.WriteLine("3. Multiplication");
-Console.WriteLine("4. Division");
-Console.WriteLine("5. Fibonacci Sequence");
-//Console.WriteLine("5. Modulus");
+// Variable Decalarations
+int choice = 0;
+int num1 = 0, num2 = 0;
 
 // Prompt for user input
-int choice = Convert.ToInt32(Console.ReadLine());
+
 
 while (choice != -1)
 {
-    Console.Write("Please enter the first number: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("Please enter the seecond number: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
-
-
-
-    // Decide which operation is needed based on selected option
-
-    //if(choice == 1)
-    //{
-    //    /* do addition */
-    //}
-    //else if(choice == 2)
-    //{
-    //    /* Subtraction */
-    //}
-    //else if (choice == 2)
-    //{
-    //    /* Multiplication */
-    //}
-    //else if (choice == 2)
-    //{
-    //    /* Division */
-    //}
-    //else
-    //{
-    //    Console.WriteLine("Invalid Choice");
-    //}
-
-    /* Switch statment */
-    int answer = 0;
-    switch (choice)
+    try
     {
-        case 1:
-            /* Addition */
-            answer = num1 + num2;
+
+        // Show calculator options / Show menu
+        Console.Clear();
+        Console.WriteLine("Please select an operation (-1 to exit program)");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Fibonacci Sequence");
+        //Console.WriteLine("5. Modulus");
+        
+        choice = Convert.ToInt32(Console.ReadLine());
+        
+        
+
+
+        if (choice == -1)
+        {
             break;
-        case 2:
-            /* Subtraction */
-            answer = num1 - num2;
-            break;
-        case 3:
-            /* Multiplication */
-            answer = num1 * num2;
-            break;
-        case 4:
-            /* Division */
-            answer = num1 / num2;
-            break;
-        case 5:
-            for (int i = num1; i <= num2; i++)
-            {
-                answer += i;
-            }
-            break;
-        default:
-            Console.WriteLine("Invalid Choice");
-            break;
+        }
+
+        if(choice < 6)
+        {
+            Console.Write("Please enter the first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the seecond number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        // Decide which operation is needed based on selected option
+
+        //if(choice == 1)
+        //{
+        //    /* do addition */
+        //}
+        //else if(choice == 2)
+        //{
+        //    /* Subtraction */
+        //}
+        //else if (choice == 2)
+        //{
+        //    /* Multiplication */
+        //}
+        //else if (choice == 2)
+        //{
+        //    /* Division */
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Invalid Choice");
+        //}
+
+        /* Switch statment */
+        int answer = 0;
+        switch (choice)
+        {
+            case 1:
+                /* Addition */
+                answer = num1 + num2;
+                break;
+            case 2:
+                /* Subtraction */
+                answer = num1 - num2;
+                break;
+            case 3:
+                /* Multiplication */
+                answer = num1 * num2;
+                break;
+            case 4:
+                /* Division */
+                answer = num1 / num2;
+                break;
+            case 5:
+                for (int i = num1; i <= num2; i++)
+                {
+                    answer += i;
+                }
+                break;
+            default:
+                throw new Exception("Invalid Menu Item Selected.");
+        }
+
+        // print output
+        Console.WriteLine($"The result is: {answer}");
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Cannot divide by zero");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        
+    }
+    finally
+    {
+        Console.WriteLine("press any key to continue.");
+        Console.ReadLine();
     }
 
-    // print output
-    Console.WriteLine($"The result is: {answer}");
-    Console.WriteLine($"Press Enter to continue");
-    Console.ReadLine();
 
-    // Show calculator options / Show menu
-    Console.WriteLine("Please select an operation (-1 to exit program)");
-    Console.WriteLine("1. Addition");
-    Console.WriteLine("2. Subtraction");
-    Console.WriteLine("3. Multiplication");
-    Console.WriteLine("4. Division");
-    Console.WriteLine("5. Fibonacci Sequence");
-    //Console.WriteLine("5. Modulus");
-
-    // Prompt for user input
-    choice = Convert.ToInt32(Console.ReadLine());
 }
 
 Console.WriteLine("Thank you for using the sample calculator program!");
